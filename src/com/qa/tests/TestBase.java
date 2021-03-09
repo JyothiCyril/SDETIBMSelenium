5772b3635fe546dabb181774e4a143b09783d86b
JyothiCyril <pjyothi.lakshmi@gmail.com>
2021-03-09 14:09:46 +0530

    Day 2 Second Commit

--- src/com/qa/tests/TestBase.java
+++ src/com/qa/tests/TestBase.java
@@ -2,8 +2,12 @@ package com.qa.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import com.qa.Pages.AmazonPages;
 
@@ -12,19 +16,35 @@ public class TestBase {
 	//	2. Run the URL
 	WebDriver driver;
 	AmazonPages AmazonOR;
-	
 
+	@Parameters({"Browser"})
 	@BeforeClass
-	public void setUp() {
+	public void setUp(String Browser) {
 
-		System.setProperty("webdriver.chrome.driver", "D:\\Selenium Software\\Drivers\\chromedriver.exe");
-		driver = new ChromeDriver();
+		if(Browser.equalsIgnoreCase("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", "D:\\Selenium Software\\Drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if(Browser.equalsIgnoreCase("Firefox")) {				
+			System.setProperty("webdriver.gecko.driver", "D:\\Selenium Software\\Drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();		
+
+		}else if(Browser.equalsIgnoreCase("Edge")) {
+			System.setProperty("webdriver.edge.driver", "D:\\Selenium Software\\Drivers\\msedgedriver.exe");
+			driver = new EdgeDriver();
+
+		}else if(Browser.equalsIgnoreCase("IE")) {
+			System.setProperty("webdriver.ie.driver", "D:\\Selenium Software\\Drivers\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+
+		}
+
+
 		AmazonOR = new AmazonPages(driver);
 		driver.get("https://www.amazon.in/");
 	}
 
 
-	
+
 	//7. Close the browser
 	@AfterClass
 	public void tearDown() {
